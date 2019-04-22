628cc5f72b53ec331fe1d5acc4433d25c816c3fe
Bontiv <prog.bontiv@gmail.com>
2019-04-23 00:38:49 +0200

    Initial release

---
+++ acs.php
@@ -0,0 +1,127 @@
+<?php
+
+
+/* Copyright (C) 2001-2005 Rodolphe Quiedeville <rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2015 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2012 Regis Houssin        <regis.houssin@capnetworks.com>
+ * Copyright (C) 2015      Jean-François Ferry	<jfefe@aternatik.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *    \file       htdocs/saml/template/login.php
+ *    \ingroup    saml
+ *    \brief      Home page of saml top menu
+ */
+
+define('NOLOGIN', true);
+define('NOCSRFCHECK', true);
+
+// Load Dolibarr environment
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME'];
+$tmp2 = realpath(__FILE__);
+$i = strlen($tmp) - 1;
+$j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+    $i--;
+    $j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1)) . "/main.inc.php")) $res = @include(substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php")) $res = @include(dirname(substr($tmp, 0, ($i + 1))) . "/main.inc.php");
+// Try main.inc.php using relative path
+if (!$res && file_exists("../main.inc.php")) $res = @include("../main.inc.php");
+if (!$res && file_exists("../../main.inc.php")) $res = @include("../../main.inc.php");
+if (!$res && file_exists("../../../main.inc.php")) $res = @include("../../../main.inc.php");
+if (!$res) die("Include of main fails");
+
+require_once DOL_DOCUMENT_ROOT . '/core/class/html.formfile.class.php';
+require_once 'lib/autoload.php';
+
+/** @var \OneLogin\Saml2\Auth $login */
+$login = get_saml();
+
+$login->processResponse();
+
+if ($login->isAuthenticated()) {
+    $user = new User($db);
+    $admin = new User($db);
+    $admin->fetch('', 'admin');
+
+    $res = $user->fetch('', $login->getNameId());
+    $user->firstname = $login->getAttribute('first_name')[0];
+    $user->lastname = $login->getAttribute('last_name')[0];
+    $user->admin = $login->getAttribute('type')[0] == 'ADMINISTRATOR' ? 1 : 0;
+    $user->email = $login->getAttribute('mail')[0];
+
+    if ($res <= 0) {
+        $user->login = $login->getNameId();
+        $user->create($admin);
+
+    } else {
+        $user->update($admin);
+    }
+
+
+    $_SESSION["dol_login"]=$user->login;
+    $_SESSION["dol_authmode"]='saml';
+    $_SESSION["dol_tz"]=isset($dol_tz)?$dol_tz:'';
+    $_SESSION["dol_tz_string"]=isset($dol_tz_string)?$dol_tz_string:'';
+    $_SESSION["dol_dst"]=isset($dol_dst)?$dol_dst:'';
+    $_SESSION["dol_dst_observed"]=isset($dol_dst_observed)?$dol_dst_observed:'';
+    $_SESSION["dol_dst_first"]=isset($dol_dst_first)?$dol_dst_first:'';
+    $_SESSION["dol_dst_second"]=isset($dol_dst_second)?$dol_dst_second:'';
+    $_SESSION["dol_screenwidth"]=isset($dol_screenwidth)?$dol_screenwidth:'';
+    $_SESSION["dol_screenheight"]=isset($dol_screenheight)?$dol_screenheight:'';
+    $_SESSION["dol_company"]=$conf->global->MAIN_INFO_SOCIETE_NOM;
+    $_SESSION["dol_entity"]=$conf->entity;
+    // Store value into session (values stored only if defined)
+    if (! empty($dol_hide_topmenu))         $_SESSION['dol_hide_topmenu']=$dol_hide_topmenu;
+    if (! empty($dol_hide_leftmenu))        $_SESSION['dol_hide_leftmenu']=$dol_hide_leftmenu;
+    if (! empty($dol_optimize_smallscreen)) $_SESSION['dol_optimize_smallscreen']=$dol_optimize_smallscreen;
+    if (! empty($dol_no_mouse_hover))       $_SESSION['dol_no_mouse_hover']=$dol_no_mouse_hover;
+    if (! empty($dol_use_jmobile))          $_SESSION['dol_use_jmobile']=$dol_use_jmobile;
+
+    dol_syslog("This is a new started user session. _SESSION['dol_login']=".$_SESSION["dol_login"]." Session id=".session_id());
+
+    $db->begin();
+    $user->update_last_login_date();
+
+    $loginfo = 'TZ='.$_SESSION["dol_tz"].';TZString='.$_SESSION["dol_tz_string"].';Screen='.$_SESSION["dol_screenwidth"].'x'.$_SESSION["dol_screenheight"];
+
+    // Call triggers for the "security events" log
+    $user->trigger_mesg = $loginfo;
+    // Call triggers
+    include_once DOL_DOCUMENT_ROOT . '/core/class/interfaces.class.php';
+    $interface=new Interfaces($db);
+    $result=$interface->run_triggers('USER_LOGIN',$user,$user,$langs,$conf);
+
+    $hookmanager->initHooks(array('login'));
+    $parameters=array('dol_authmode'=>'saml', 'dol_loginfo'=>$loginfo);
+    $reshook=$hookmanager->executeHooks('afterLogin',$parameters,$user,$action);    // Note that $action and $object may have been modified by some hooks
+
+    $db->commit();
+    header('Location: ' . DOL_URL_ROOT);
+}
+
+if (isset($_REQUEST['RelayState'])) {
+    $login->redirectTo($_REQUEST['RelayState']);
+}
+?>
+<a href="login.php">Login</a>
+<a href="logout.php">Logout</a>
---
+++ lib/saml_settings.php
@@ -0,0 +1,292 @@
+<?php
+
+use OneLogin\Saml2\IdPMetadataParser;
+use OneLogin\Saml2\Constants;
+use OneLogin\Saml2\Auth;
+
+/**
+ * @return array
+ */
+function saml_settings()
+{
+    $saml_url_root = DOL_MAIN_URL_ROOT . '/custom/saml/';
+
+    $settings = array(
+        // If 'strict' is True, then the PHP Toolkit will reject unsigned
+        // or unencrypted messages if it expects them to be signed or encrypted.
+        // Also it will reject the messages if the SAML standard is not strictly
+        // followed: Destination, NameId, Conditions ... are validated too.
+        'strict' => false,
+
+        // Enable debug mode (to print errors).
+        'debug' => false,
+
+        // Set a BaseURL to be used instead of try to guess
+        // the BaseURL of the view that process the SAML Message.
+        // Ex http://sp.example.com/
+        //    http://example.com/sp/
+        'baseurl' => null,
+
+        // Service Provider Data that we are deploying.
+        'sp' => array(
+            // Identifier of the SP entity  (must be a URI)
+            'entityId' => 'crm.epitanime.com',
+            // Specifies info about where and how the <AuthnResponse> message MUST be
+            // returned to the requester, in this case our SP.
+            'assertionConsumerService' => array(
+                // URL Location where the <Response> from the IdP will be returned
+                'url' => $saml_url_root . 'acs.php',
+                // SAML protocol binding to be used when returning the <Response>
+                // message. OneLogin Toolkit supports this endpoint for the
+                // HTTP-POST binding only.
+                'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+            ),
+            // If you need to specify requested attributes, set a
+            // attributeConsumingService. nameFormat, attributeValue and
+            // friendlyName can be omitted
+            "attributeConsumingService" => array(
+                "serviceName" => "Dolibarr CRM",
+                "serviceDescription" => "ERP for small enterprises",
+                "requestedAttributes" => array(
+                    array(
+                        "name" => "mail",
+                        "isRequired" => true,
+                        "nameFormat" => "",
+                        "friendlyName" => "User email address",
+                        "attributeValue" => array()
+                    ),
+                    array(
+                        "name" => "uid",
+                        "isRequired" => false,
+                        "nameFormat" => "",
+                        "friendlyName" => "User name",
+                        "attributeValue" => array()
+                    )
+                )
+            ),
+            // Specifies info about where and how the <Logout Response> message MUST be
+            // returned to the requester, in this case our SP.
+            'singleLogoutService' => array(
+                // URL Location where the <Response> from the IdP will be returned
+                'url' => $saml_url_root . 'sls.php',
+                // SAML protocol binding to be used when returning the <Response>
+                // message. OneLogin Toolkit supports the HTTP-Redirect binding
+                // only for this endpoint.
+                'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+            ),
+            // Specifies the constraints on the name identifier to be used to
+            // represent the requested subject.
+            // Take a look on lib/Saml2/Constants.php to see the NameIdFormat supported.
+            'NameIDFormat' => Constants::NAMEID_EMAIL_ADDRESS,
+            // Usually x509cert and privateKey of the SP are provided by files placed at
+            // the certs folder. But we can also provide them with the following parameters
+            'x509cert' => '',
+            'privateKey' => '',
+
+            /*
+             * Key rollover
+             * If you plan to update the SP x509cert and privateKey
+             * you can define here the new x509cert and it will be
+             * published on the SP metadata so Identity Providers can
+             * read them and get ready for rollover.
+             */
+            // 'x509certNew' => '',
+        ),
+
+        // Identity Provider Data that we want connected with our SP.
+        'idp' => array(
+            // Identifier of the IdP entity  (must be a URI)
+            'entityId' => '',
+            // SSO endpoint info of the IdP. (Authentication Request protocol)
+            'singleSignOnService' => array(
+                // URL Target of the IdP where the Authentication Request Message
+                // will be sent.
+                'url' => '',
+                // SAML protocol binding to be used when returning the <Response>
+                // message. OneLogin Toolkit supports the HTTP-Redirect binding
+                // only for this endpoint.
+                'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+            ),
+            // SLO endpoint info of the IdP.
+            'singleLogoutService' => array(
+                // URL Location of the IdP where SLO Request will be sent.
+                'url' => '',
+                // URL location of the IdP where the SP will send the SLO Response (ResponseLocation)
+                // if not set, url for the SLO Request will be used
+                'responseUrl' => '',
+                // SAML protocol binding to be used when returning the <Response>
+                // message. OneLogin Toolkit supports the HTTP-Redirect binding
+                // only for this endpoint.
+                'binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+            ),
+            // Public x509 certificate of the IdP
+            'x509cert' => '',
+            /*
+             *  Instead of use the whole x509cert you can use a fingerprint in order to
+             *  validate a SAMLResponse, but we don't recommend to use that
+             *  method on production since is exploitable by a collision attack.
+             *  (openssl x509 -noout -fingerprint -in "idp.crt" to generate it,
+             *   or add for example the -sha256 , -sha384 or -sha512 parameter)
+             *
+             *  If a fingerprint is provided, then the certFingerprintAlgorithm is required in order to
+             *  let the toolkit know which algorithm was used. Possible values: sha1, sha256, sha384 or sha512
+             *  'sha1' is the default value.
+             *
+             *  Notice that if you want to validate any SAML Message sent by the HTTP-Redirect binding, you
+             *  will need to provide the whole x509cert.
+             */
+            // 'certFingerprint' => '',
+            // 'certFingerprintAlgorithm' => 'sha1',
+
+            /* In some scenarios the IdP uses different certificates for
+             * signing/encryption, or is under key rollover phase and
+             * more than one certificate is published on IdP metadata.
+             * In order to handle that the toolkit offers that parameter.
+             * (when used, 'x509cert' and 'certFingerprint' values are
+             * ignored).
+             */
+            // 'x509certMulti' => array(
+            //      'signing' => array(
+            //          0 => '<cert1-string>',
+            //      ),
+            //      'encryption' => array(
+            //          0 => '<cert2-string>',
+            //      )
+            // ),
+        ),
+    );
+
+
+    $idp_metadata = IdPMetadataParser::parseRemoteXML('https://account.epitanime.com/saml2/idp/metadata.php');
+    $settings_compiled = IdPMetadataParser::injectIntoSettings($settings, $idp_metadata);
+    $settings_compiled['sp'] = $settings['sp'];
+    return $settings_compiled;
+};
+
+
+/**
+ * Get SAML Auth object
+ * @return Auth
+ * @throws \OneLogin\Saml2\Error
+ */
+function get_saml() {
+    return new Auth(saml_settings());
+}
+
+$advancedSettings = array (
+
+    // Compression settings
+    'compress' => array (
+        'requests' => true,
+        'responses' => true
+    ),
+    // Security settings
+    'security' => array (
+
+        /** signatures and encryptions offered */
+
+        // Indicates that the nameID of the <samlp:logoutRequest> sent by this SP
+        // will be encrypted.
+        'nameIdEncrypted' => false,
+
+        // Indicates whether the <samlp:AuthnRequest> messages sent by this SP
+        // will be signed.  [Metadata of the SP will offer this info]
+        'authnRequestsSigned' => false,
+
+        // Indicates whether the <samlp:logoutRequest> messages sent by this SP
+        // will be signed.
+        'logoutRequestSigned' => false,
+
+        // Indicates whether the <samlp:logoutResponse> messages sent by this SP
+        // will be signed.
+        'logoutResponseSigned' => false,
+
+        /* Sign the Metadata
+         False || True (use sp certs) || array (
+                                                    keyFileName => 'metadata.key',
+                                                    certFileName => 'metadata.crt'
+                                                )
+        */
+        'signMetadata' => false,
+
+        /** signatures and encryptions required **/
+
+        // Indicates a requirement for the <samlp:Response>, <samlp:LogoutRequest>
+        // and <samlp:LogoutResponse> elements received by this SP to be signed.
+        'wantMessagesSigned' => false,
+
+        // Indicates a requirement for the <saml:Assertion> elements received by
+        // this SP to be encrypted.
+        'wantAssertionsEncrypted' => false,
+
+        // Indicates a requirement for the <saml:Assertion> elements received by
+        // this SP to be signed. [Metadata of the SP will offer this info]
+        'wantAssertionsSigned' => false,
+
+        // Indicates a requirement for the NameID element on the SAMLResponse
+        // received by this SP to be present.
+        'wantNameId' => true,
+
+        // Indicates a requirement for the NameID received by
+        // this SP to be encrypted.
+        'wantNameIdEncrypted' => false,
+
+        // Authentication context.
+        // Set to false and no AuthContext will be sent in the AuthNRequest.
+        // Set true or don't present this parameter and you will get an AuthContext 'exact' 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'.
+        // Set an array with the possible auth context values: array ('urn:oasis:names:tc:SAML:2.0:ac:classes:Password', 'urn:oasis:names:tc:SAML:2.0:ac:classes:X509').
+        'requestedAuthnContext' => false,
+
+        // Indicates if the SP will validate all received xmls.
+        // (In order to validate the xml, 'strict' and 'wantXMLValidation' must be true).
+        'wantXMLValidation' => true,
+
+        // If true, SAMLResponses with an empty value at its Destination
+        // attribute will not be rejected for this fact.
+        'relaxDestinationValidation' => false,
+
+        // Algorithm that the toolkit will use on signing process. Options:
+        //    'http://www.w3.org/2000/09/xmldsig#rsa-sha1'
+        //    'http://www.w3.org/2000/09/xmldsig#dsa-sha1'
+        //    'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
+        //    'http://www.w3.org/2001/04/xmldsig-more#rsa-sha384'
+        //    'http://www.w3.org/2001/04/xmldsig-more#rsa-sha512'
+        // Notice that sha1 is a deprecated algorithm and should not be used
+        'signatureAlgorithm' => 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
+
+        // Algorithm that the toolkit will use on digest process. Options:
+        //    'http://www.w3.org/2000/09/xmldsig#sha1'
+        //    'http://www.w3.org/2001/04/xmlenc#sha256'
+        //    'http://www.w3.org/2001/04/xmldsig-more#sha384'
+        //    'http://www.w3.org/2001/04/xmlenc#sha512'
+        // Notice that sha1 is a deprecated algorithm and should not be used
+        'digestAlgorithm' => 'http://www.w3.org/2001/04/xmlenc#sha256',
+
+        // ADFS URL-Encodes SAML data as lowercase, and the toolkit by default uses
+        // uppercase. Turn it True for ADFS compatibility on signature verification
+        'lowercaseUrlencoding' => false,
+    ),
+
+    // Contact information template, it is recommended to supply a
+    // technical and support contacts.
+//    'contactPerson' => array (
+//        'technical' => array (
+//            'givenName' => '',
+//            'emailAddress' => ''
+//        ),
+//        'support' => array (
+//            'givenName' => '',
+//            'emailAddress' => ''
+//        ),
+//    ),
+
+    // Organization information template, the info in en_US lang is
+    // recomended, add more if required.
+//    'organization' => array (
+//        'en-US' => array(
+//            'name' => '',
+//            'displayname' => '',
+//            'url' => ''
+//        ),
+//    ),
+);
